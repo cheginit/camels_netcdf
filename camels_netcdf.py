30d20dff1dcb13cb7adb739320c9c4f11ccb1d7e
cheginit <cheginit@gmail.com>
2022-02-28 17:44:52 -0600

    Run pre-commit

--- camels_netcdf.py
+++ camels_netcdf.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python
 """Convert CAMELS data to netcdf/feather format."""
-from typing import Tuple
-import pandas as pd
-from pathlib import Path
-import xarray as xr
-import geopandas as gpd
 import warnings
+from pathlib import Path
+from typing import Tuple
 
-warnings.filterwarnings('ignore', message='.*initial implementation of Parquet.*')
+import geopandas as gpd
+import pandas as pd
+import xarray as xr
+
+warnings.filterwarnings("ignore", message=".*initial implementation of Parquet.*")
 
 ATTR_DIR = Path("data", "camels_attributes_v2.0")
 BASIN_DIR = Path("data", "basin_set_full_res")
-QOBS_DIR = Path("data", "basin_timeseries_v1p2_metForcing_obsFlow", "basin_dataset_public_v1p2", "usgs_streamflow")
+QOBS_DIR = Path(
+    "data",
+    "basin_timeseries_v1p2_metForcing_obsFlow",
+    "basin_dataset_public_v1p2",
+    "usgs_streamflow",
+)
+
 
 def read_basin() -> gpd.GeoDataFrame:
     """Read the basin shapefile."""
@@ -21,13 +28,20 @@ def read_basin() -> gpd.GeoDataFrame:
     basin["hru_id"] = basin.hru_id.astype(str).str.zfill(8)
     return basin.set_index("hru_id").geometry
 
+
 def read_attributes() -> Tuple[pd.DataFrame, pd.Index]:
     """Convert all the attributes to a single dataframe."""
     print("Reading basin attributes ...")
     attr_files = Path(ATTR_DIR).glob("camels_*.txt")
-    attrs = {f.stem.split("_")[1]: pd.read_csv(f, sep=";", index_col=0, dtype={"huc_02": str, "gauge_id": str}) for f in attr_files}
+    attrs = {
+        f.stem.split("_")[1]: pd.read_csv(
+            f, sep=";", index_col=0, dtype={"huc_02": str, "gauge_id": str}
+        )
+        for f in attr_files
+    }
 
     attrs_df = pd.concat(attrs.values(), axis=1)
+
     def fix_station_nm(station_nm):
         name = station_nm.title().rsplit(" ", 1)
         name[0] = name[0] if name[0][-1] == "," else f"{name[0]},"
@@ -38,31 +52,36 @@ def read_attributes() -> Tuple[pd.DataFrame, pd.Index]:
     obj_cols = attrs_df.columns[attrs_df.dtypes == "object"]
     for c in obj_cols:
         attrs_df[c] = attrs_df[c].str.strip().astype(str)
-    
+
     basin = read_basin()
     return gpd.GeoDataFrame(attrs_df, geometry=basin, crs="epsg:4326"), obj_cols
+
 
 def read_qobs(qobs_txt: Path) -> pd.DataFrame:
     """Read the streamflow data."""
     qobs = pd.read_csv(qobs_txt, delim_whitespace=True, header=None, index_col=0, dtype={0: str})
     qobs = qobs.rename(columns={1: "Year", 2: "Month", 3: "Day", 4: qobs.index[0], 5: "Quality"})
-    qobs["time"] = pd.to_datetime(qobs[['Year', 'Month', 'Day']])
+    qobs["time"] = pd.to_datetime(qobs[["Year", "Month", "Day"]])
     qobs = qobs.drop(columns=["Year", "Month", "Day", "Quality"])
     qobs = qobs.set_index("time")
     return qobs
+
 
 attrs, obj_cols = read_attributes()
 attrs.to_feather("camels_attributes_v2.0.feather")
 
 print("Reading basin streamflow data ...")
-qobs = pd.concat((read_qobs(Path(QOBS_DIR, huc, f"{sid}_streamflow_qc.txt")) for sid, huc in attrs["huc_02"].items()), axis=1)
+qobs = pd.concat(
+    (
+        read_qobs(Path(QOBS_DIR, huc, f"{sid}_streamflow_qc.txt"))
+        for sid, huc in attrs["huc_02"].items()
+    ),
+    axis=1,
+)
 ds = xr.Dataset(
     data_vars={
         "discharge": (["time", "station_id"], qobs),
-        **{
-            attr: (["station_id"], v)
-            for attr, v in attrs.drop(columns="geometry").items()
-        },
+        **{attr: (["station_id"], v) for attr, v in attrs.drop(columns="geometry").items()},
     },
     coords={
         "time": qobs.index.to_numpy(),
